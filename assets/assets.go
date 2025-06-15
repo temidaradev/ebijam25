c562603300e8c75e84298ecfd6d7923283d11e06
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-06-15 18:17:22 +0300

    esset and main font

--- assets/assets.go
+++ assets/assets.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
 
-//go:embed font/DMSans.ttf
+//go:embed font/PublicPixel.ttf
 var Font []byte
 var FontFaceS text.Face
 var FontFaceM text.Face
--- src/main.go
+++ src/main.go
@@ -2,9 +2,17 @@ package src
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/temidaradev/ebijam25/assets"
+	"github.com/temidaradev/esset/v2"
+	"image/color"
 )
 
 type Game struct {
+}
+
+func init() {
+	assets.FontFaceS, _ = esset.GetFont(assets.Font, 16)
+	assets.FontFaceM, _ = esset.GetFont(assets.Font, 32)
 }
 
 func NewGame() *Game {
@@ -16,7 +24,7 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	esset.DrawText(screen, "Esset\nBasic Asset Implementer\nFor Ebitengine!", 0, 75, assets.FontFaceM, color.White)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
